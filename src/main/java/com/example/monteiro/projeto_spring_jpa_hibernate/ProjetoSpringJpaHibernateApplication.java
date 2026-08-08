19e7b9104af724f3e439d6246c6c5fedd216cecd
simaomonteiro18 <sasmonteiro07@gmail.com>
2026-08-08 22:52:21 +0100

    Project created

---
+++ src/main/java/com/example/monteiro/projeto_spring_jpa_hibernate/ProjetoSpringJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.monteiro.projeto_spring_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/monteiro/projeto_spring_jpa_hibernate/ProjetoSpringJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monteiro.projeto_spring_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
