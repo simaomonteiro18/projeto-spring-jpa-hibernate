7e4045624c7190b1d30573da97d26d4683481a8f
simaomonteiro18 <sasmonteiro07@gmail.com>
2026-08-09 14:21:46 +0100

    H2 Database, Test Profile and JPA

--- src/main/java/com/example/monteiro/projeto_spring_jpa_hibernate/entities/User.java
+++ src/main/java/com/example/monteiro/projeto_spring_jpa_hibernate/entities/User.java
@@ -1,10 +1,16 @@
 package com.example.monteiro.projeto_spring_jpa_hibernate.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
