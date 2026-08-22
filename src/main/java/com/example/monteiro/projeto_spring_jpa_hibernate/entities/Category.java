406b198ded0481e1e14cbf7cd9e9a968019fc3db
simaomonteiro18 <sasmonteiro07@gmail.com>
2026-08-22 13:21:16 +0100

    Many-To-Many association with JoinTable

--- src/main/java/com/example/monteiro/projeto_spring_jpa_hibernate/entities/Category.java
+++ src/main/java/com/example/monteiro/projeto_spring_jpa_hibernate/entities/Category.java
@@ -1,5 +1,6 @@
 package com.example.monteiro.projeto_spring_jpa_hibernate.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,6 +17,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
